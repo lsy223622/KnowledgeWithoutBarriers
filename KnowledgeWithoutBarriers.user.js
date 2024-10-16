c85be14f481baf914aa17333dc2aa8eced54b423
lsy223622 <lsy223622@outlook.com>
2024-10-17 00:05:16 +0800

    Remove watermark immediately once loaded

--- KnowledgeWithoutBarriers.user.js
+++ KnowledgeWithoutBarriers.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         移除录直播应用视频上的水印
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  移除录直播应用视频上的水印 (#myVideos > div.mark-content 和 #myVideos1 > div.mark-content)
+// @version      0.3
+// @description  使用 MutationObserver 在水印刚出现时立即移除 (#myVideos > div.mark-content 和 #myVideos1 > div.mark-content)
 // @match        http://newesxidian.chaoxing.com/*
 // @grant        none
 // ==/UserScript==
@@ -10,25 +10,41 @@
 (function () {
   "use strict";
 
-  function removeElements() {
-    const selectors = ["#myVideos > div.mark-content", "#myVideos1 > div.mark-content"];
+  const selectors = ["#myVideos > div.mark-content", "#myVideos1 > div.mark-content"];
 
+  function removeElements() {
     selectors.forEach((selector) => {
       const elementToRemove = document.querySelector(selector);
       if (elementToRemove) {
         elementToRemove.remove();
         console.log(`水印已被移除：${selector}`);
-      } else {
-        console.log(`未找到要移除的水印：${selector}`);
       }
     });
   }
 
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", removeElements);
-  } else {
-    removeElements();
+  function observeDOMChanges() {
+    const targetNode = document.body;
+    const config = { childList: true, subtree: true };
+
+    const callback = function (mutationsList) {
+      for (let mutation of mutationsList) {
+        if (mutation.type === "childList") {
+          removeElements();
+        }
+      }
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
   }
 
-  setInterval(removeElements, 1000);
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", () => {
+      removeElements();
+      observeDOMChanges();
+    });
+  } else {
+    removeElements();
+    observeDOMChanges();
+  }
 })();
