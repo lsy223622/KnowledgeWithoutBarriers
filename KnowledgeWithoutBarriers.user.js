7e3c2012cd8a31cf87dfce9f890173600af8a0ae
lsy223622 <lsy223622@outlook.com>
2024-10-17 00:43:00 +0800

    Rename

--- KnowledgeWithoutBarriers.user.js
+++ KnowledgeWithoutBarriers.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         移除录直播应用视频上的水印
+// @name         移除录直播平台视频上的水印
 // @namespace    http://tampermonkey.net/
 // @version      0.6
 // @description  使用 MutationObserver 在水印刚出现时立即移除 (#myVideos > div.mark-content 和 #myVideos1 > div.mark-content)
